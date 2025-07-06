5ab29220d57b7a93960a4fe21499a86fe455b46c
Jaime Pajares <jaimepajares@gmail.com>
2025-07-05 22:00:07 -0600

    feat separation of dependencies between school.dto and school.type

---
+++ src/core/school/dto/school.type.ts
@@ -0,0 +1,31 @@
+import { School } from "@prisma/client";
+
+export type ICreateSchool = Pick<
+  School,
+  "name" | "sector" | "districtId" | "address" | "email" | "coordenates" | "phoneNumber" | "createdBy"
+>;
+
+export type IUpdateSchool = Pick<
+  School,
+  | "id"
+  | "name"
+  | "sector"
+  | "districtId"
+  | "address"
+  | "email"
+  | "coordenates"
+  | "phoneNumber"
+  | "updatedBy"
+>;
+
+export type IDeleteSchool = Pick<School, "id" | "deletedBy">;
+
+export interface IGetSchool
+  extends Pick<
+    School,
+    "id" | "name" | "sector" | "districtId" | "address" | "email" | "coordenates" | "phoneNumber"
+  > {
+  _count: {
+    PrincipalSchool: number;
+  };
+}
