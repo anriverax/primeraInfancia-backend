ec80714ebc20a4594da6908525d52d35e8cbb539
Jaime Pajares <jaimepajares@gmail.com>
2025-07-18 21:33:09 -0600

    fix change order by properties

--- src/core/school/cqrs/queries/findUnique/getByIdSchool.handler.ts
+++ src/core/school/cqrs/queries/findUnique/getByIdSchool.handler.ts
@@ -5,61 +5,108 @@ import { IGetSchool } from "@/core/school/dto/school.type";
 
 @QueryHandler(GetByIdSchoolQuery)
 export class GetByIdSchoolHandler {
-  constructor(private readonly prisma: PrismaService) { }
+  constructor(private readonly prisma: PrismaService) {}
+
+  // async execute(query: GetByIdSchoolQuery): Promise<IGetSchool | null> {
+  //   const schools = await this.prisma.school.findUnique({
+  //     where: { id: query.id },
+  //     select: {
+  //       id: true,
+  //       name: true,
+  //       sector: true,
+  //       districtId: true,
+  //       address: true,
+  //       email: true,
+  //       coordenates: true,
+  //       phoneNumber: true,
+  //       District: {
+  //         select: { name: true }
+  //       },
+  //       PrincipalSchool: {
+  //         select: {
+  //           personId: true,
+  //           Person: {
+  //             select: {
+  //               id: true,
+  //               firstName: true,
+  //               lastName1: true,
+  //               lastName2: true,
+  //               dui: true,
+  //               phoneNumber: true,
+  //               User: {
+  //                 select: {
+  //                   email: true
+  //                 }
+  //               },
+  //               TypePerson: {
+  //                 select: { name: true }
+  //               }
+  //             }
+  //           }
+  //         }
+  //       },
+  //       _count: {
+  //         select: {
+  //           PrincipalSchool: true
+  //         }
+  //       }
+  //     }
+  //   });
+
+  //   return schools;
+  // }
 
   async execute(query: GetByIdSchoolQuery): Promise<IGetSchool | null> {
-    const schools = await this.prisma.school.findUnique({
-      where: { id: query.id },
-      select: {
-        id: true,
-        name: true,
-        sector: true,
-        districtId: true,
-        address: true,
-        email: true,
-        coordenates: true,
-        phoneNumber: true,
-        District: {
-          select: { name: true },
-        },
-        PrincipalSchool: {
-          select: {
-            personId: true,
-            Person: {
-              select: {
-                id: true,
-                firstName: true,
-                lastName1: true,
-                lastName2: true,
-                dui: true,
-                phoneNumber: true,
-                User: {
-                  select: {
-                    email: true,
-                  },
+  const schools = await this.prisma.school.findUnique({
+    where: { id: query.id },
+    select: {
+      id: true,
+      name: true,
+      sector: true,
+      districtId: true,
+      address: true,
+      email: true,
+      coordenates: true,
+      phoneNumber: true,
+      District: {
+        select: { name: true },
+      },
+      PrincipalSchool: {
+        select: {
+          personId: true,
+          Person: {
+            select: {
+              id: true,
+              firstName: true,
+              lastName1: true,
+              lastName2: true,
+              dui: true,
+              phoneNumber: true,
+              User: {
+                select: {
+                  email: true,
                 },
-                TypePerson: {
-                  select: { name: true },
-                },
+              },
+              TypePerson: {
+                select: { name: true },
               },
             },
           },
-          orderBy: {
-            Person: {
-              firstName: 'asc',
-              lastName1: 'asc',
-              lastName2: 'asc'
-            },
-          },
         },
-        _count: {
-          select: {
-            PrincipalSchool: true,
+        orderBy: {
+          Person: {
+            firstName: 'asc', //TODO change by fullName
           },
         },
       },
-    });
+      _count: {
+        select: {
+          PrincipalSchool: true,
+        },
+      },
+    },
+  });
 
-    return schools;
-  }
+  return schools;
+}
 }
