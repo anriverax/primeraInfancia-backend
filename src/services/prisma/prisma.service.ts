8fe7544cacd2ec0f280b883e648290d2e86d49c4
Jaime Pajares <jaimepajares@gmail.com>
2025-07-27 23:22:21 -0600

    feat enabled softDelete GroupMentor

--- src/services/prisma/prisma.service.ts
+++ src/services/prisma/prisma.service.ts
@@ -8,7 +8,7 @@ const modelsWithSoftDelete = [
   "TrainingModule",
   "EvaluationInstrument",
   "PersonRole",
-  "GroupTrainer"
+  "GroupTrainer", "GroupMentor"
 ];
 
 const prismaWithExtension = new PrismaClient().$extends({
