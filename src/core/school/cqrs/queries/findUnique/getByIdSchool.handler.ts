4d146327957bd89533833115805308af25d2a259
Anibal Rivera <79974791+anriverax@users.noreply.github.com>
2025-07-27 23:26:41 -0600

    Update getByIdSchool.handler.ts

--- src/core/school/cqrs/queries/findUnique/getByIdSchool.handler.ts
+++ src/core/school/cqrs/queries/findUnique/getByIdSchool.handler.ts
@@ -7,55 +7,6 @@ import { IGetSchool } from "@/core/school/dto/school.type";
 export class GetByIdSchoolHandler {
   constructor(private readonly prisma: PrismaService) {}
 
-  // async execute(query: GetByIdSchoolQuery): Promise<IGetSchool | null> {
-  //   const schools = await this.prisma.school.findUnique({
-  //     where: { id: query.id },
-  //     select: {
-  //       id: true,
-  //       name: true,
-  //       sector: true,
-  //       districtId: true,
-  //       address: true,
-  //       email: true,
-  //       coordenates: true,
-  //       phoneNumber: true,
-  //       District: {
-  //         select: { name: true }
-  //       },
-  //       PrincipalSchool: {
-  //         select: {
-  //           personId: true,
-  //           Person: {
-  //             select: {
-  //               id: true,
-  //               firstName: true,
-  //               lastName1: true,
-  //               lastName2: true,
-  //               dui: true,
-  //               phoneNumber: true,
-  //               User: {
-  //                 select: {
-  //                   email: true
-  //                 }
-  //               },
-  //               TypePerson: {
-  //                 select: { name: true }
-  //               }
-  //             }
-  //           }
-  //         }
-  //       },
-  //       _count: {
-  //         select: {
-  //           PrincipalSchool: true
-  //         }
-  //       }
-  //     }
-  //   });
-
-  //   return schools;
-  // }
-
   async execute(query: GetByIdSchoolQuery): Promise<IGetSchool | null> {
     const schools = await this.prisma.school.findUnique({
       where: { id: query.id },
