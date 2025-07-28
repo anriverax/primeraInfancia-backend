b9e0007cc03012a6dd552eb5f44fc9600d195572
Jaime Pajares <jaimepajares@gmail.com>
2025-07-27 19:20:02 -0600

    feat enabled softDelete at PersonRole

--- src/services/prisma/prisma.service.ts
+++ src/services/prisma/prisma.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { Prisma, PrismaClient } from "@prisma/client";
 
-const modelsWithSoftDelete = ["Group","TrainingModule", "EvaluationInstrument"];
+const modelsWithSoftDelete = ["Group","TrainingModule", "EvaluationInstrument", "PersonRole"];
 
 const prismaWithExtension = new PrismaClient().$extends({
   result: {
