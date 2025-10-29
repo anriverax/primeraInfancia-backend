3c260a2d31cb247eab7432a2960f9b50f52abdb8
Aníbal Rivera <anriverax@gmail.com>
2025-10-29 17:16:22 -0600

    update x2

--- src/core/group/group.module.ts
+++ src/core/group/group.module.ts
@@ -5,7 +5,7 @@ import { Module } from "@nestjs/common";
 import { GetByIdGroupHandler } from "./cqrs/queries/findUnique/getByIdGroup.handler";
 import { GetAllGroupPaginationHandler } from "./cqrs/queries/getAllGroupPagination.query";
 import { GroupService } from "./services/group.service";
-import { GetGroupByUserHandler } from "./cqrs/queries/getGroupByUser/getGroupByUser.handler";
+import { GetGroupByUserHandler } from "./cqrs/queries/getGroupByUser/getGroupByUser.query";
 
 const CommandHandlers = [];
 const QueryHandlers = [GetByIdGroupHandler, GetAllGroupPaginationHandler, GetGroupByUserHandler];
